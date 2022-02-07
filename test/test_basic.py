da6a58be218fa5a48d38bea41fb7141c27beda3a
till-m <tmuser@ethz.ch>
2022-02-07 09:07:27 +0100

    Add two-parameter gaussian fisher information test

--- test/test_basic.py
+++ test/test_basic.py
@@ -21,7 +21,28 @@ def test_fisher_binomial():
     p = np.array([p_func(x_i) for x_i in x])
     dp = p * np.array([dp_factor(x_i) for x_i in x])
 
-    fisher_info_binomial = n / (theta * (1 - theta))
+    fisher_info_binomial = np.array([n / (theta * (1 - theta))])  # analytical
+
+    # Requires high abstol
     assert empirical_fisher(p, dp) == pytest.approx(fisher_info_binomial, 0.1)
 
-# TODO: Test fisher against multivariate normal
+
+def test_fisher_gaussian():
+    n_sample = 1000
+    np.random.seed(42)
+    mu = np.random.rand() * 5
+    nu = np.random.rand()
+
+    x = np.random.normal(mu, nu, n_sample)
+    p = 1 / (2 * np.pi * nu) * np.exp(-(x - mu)**2 / (2 * nu))
+    dpdmu = p * (x - mu) / nu
+    dpdnu = p * ((x - mu)**2 / (2 * nu**2) - 1 / (2 * nu))
+
+    dp = np.vstack([dpdmu, dpdnu]).T
+
+    # analytical
+    fisher_info_gaussian = np.array([[1 / nu, 0], [0, 1 / (2 * nu**2)]])
+    res = empirical_fisher(p, dp)
+
+    # Requires high abstol
+    assert res == pytest.approx(fisher_info_gaussian, abs=0.1)
--- triple_e/effective_dimension.py
+++ triple_e/effective_dimension.py
@@ -1,13 +1,13 @@
 import numpy as np
-from icecream import ic
+
 
 def empirical_fisher(p, dp):
     """Calculates the empirical fisher informatiom matrix of some distribution.
 
-    For some distribution p, some d distribution parameters theta, this
-    function calculates the empirical fisher information matrix over k
-    datapoints (x_i, y_i) using the pdf value at theta p(x_i, y_i; theta) and
-    it's derivate d/dtheta p(x_i, y_i; theta).
+    For some distribution p, some d-dimensional vector of distribution
+    parameters theta, this function calculates the empirical fisher information
+    matrix over k datapoints (x_i, y_i) using the pdf value at theta
+    p(x_i, y_i; theta) and it's derivate d/dtheta p(x_i, y_i; theta).
 
     Args:
         p: np.ndarray of shape (k,) with entries [p_1, p_2, ..., p_k] where
@@ -18,6 +18,12 @@ def empirical_fisher(p, dp):
                ..., ...,  ..., ....
              [J_k1, J_k2, ..., J_kd]]        
             where J_ji = d/dtheta_j p(x_i, y_i; theta)
+    
+    Returns:
+        Empirical Fisher information matrix, np.ndarray of shape (d, d).
+
+    Remarks:
+        Bad convergence behaviour, expect high uncertainty.
     """
     k = np.size(p)
 
@@ -27,9 +33,9 @@ def empirical_fisher(p, dp):
         sum = 0
         for i in range(k):
             sum += 1 / p[i]**2 * dp[i]**2
-        return sum / k
+        return np.array([sum / k])
 
     sum = np.zeros((d, d))
     for i in range(k):
-        sum += 1 / p[i]**2 * dp[:, i] @ dp[:, i].T
+        sum += 1 / p[i]**2 * np.outer(dp[i], dp[i])
     return sum / k
