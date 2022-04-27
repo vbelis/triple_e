02973c060ee175d0f4b485dbeb198054a0d2b60c
till-m <tmuser@ethz.ch>
2022-04-27 15:33:46 +0200

    Expressibility: Add `full` method, optionally return histogram values.

--- src/triple_e/expressibility.py
+++ src/triple_e/expressibility.py
@@ -4,17 +4,15 @@
 import numpy as np
 from qiskit.quantum_info import state_fidelity
 
-# number of threads to use
-#import os
-#os.environ['OMP_NUM_THREADS'] = '2'
-
 
 def expressibility(circuit_simulator,
                    n_params,
                    n_qubits,
+                   method="pairwise",
                    n_shots=1000,
                    seed=None,
-                   n_bins=None,
+                   n_bins=75,
+                   return_histogram=False,
                    epstol=1e-18):
     """Computes expressibility for a circuit.
 
@@ -24,7 +22,17 @@ def expressibility(circuit_simulator,
         n_params: The number of parameters circuit_simulator accepts. Presumed
             to be uniformly distributed in [0, 2pi]
         n_qubits: Number of qubits of the circuit.
-        n_shots: How often the simulation should be repeated.
+        method: Method to use to estimate fidelity:
+            "pairwise": Generates two samples, calculates their fidelity.
+                Computationally expensive, but memory efficient.
+            "full": Generates approximately sqrt(n_shots) samples, calculates
+                their fidelity pairwise. Computationally efficient, but memory
+                expensive.
+        n_shots: How many fidelity samples to generate.
+        n_bins: Number of equal-width bins.
+        return_histogram: If `True`, additionally returns a tuple
+            `(p_haar, p_circuit)`containing the normalized histogram data of the fidelity
+            distributions.
 
     Returns:
         The expressiblity of the circuit.
@@ -35,15 +43,31 @@ def expressibility(circuit_simulator,
     if n_bins is None:
         n_bins = 75  # as used in the paper
     n_shots = int(n_shots)
+    n_bins = int(n_bins)
 
     # estimate fidelities
     fidelities = []
-    for _ in range(n_shots):
-        params = np.random.rand(2, n_params) * 2 * np.pi
-        rho1 = circuit_simulator(params[0])
-        rho2 = circuit_simulator(params[1])
 
-        fidelities.append(state_fidelity(rho1, rho2))
+    if method == "pairwise":
+        for _ in range(n_shots):
+            params = np.random.rand(2, n_params) * 2 * np.pi
+            rho1 = circuit_simulator(params[0])
+            rho2 = circuit_simulator(params[1])
+
+            fidelities.append(state_fidelity(rho1, rho2))
+    elif method == "full":
+        samples = []
+        while len(fidelities) < n_shots:
+            params = np.random.rand(n_params) * 2 * np.pi
+            rho = circuit_simulator(params)
+
+            for smpl in samples:
+                fidelities.append(state_fidelity(rho, smpl))
+                if len(fidelities) >= n_shots:
+                    break
+            samples.append(rho)
+    else:
+        raise ValueError("Invalid argument for method provided.")
 
     # Convert fidelities to a histogram
     binning = np.linspace(0, 1, n_bins + 1)
@@ -62,5 +86,8 @@ def expressibility(circuit_simulator,
         value = fids[i]
         if (value > epstol) and (P_haar[i] > epstol):
             D_kl += value * np.log(value / P_haar[i])
+
+    if return_histogram:
+        return D_kl, (P_haar, fids)
 
     return D_kl
--- test/pennylane/test_expressibility.py
+++ test/pennylane/test_expressibility.py
@@ -51,3 +51,18 @@ def test_expressibility_single_qubit_c():
         return qml.density_matrix(0)
 
     assert one_qubit_expr(circuit_c, 3, 1000) == pytest.approx(0.007, abs=0.1)
+
+
+def test_expressibility_full_method():
+
+    def circuit_c(weights):
+        qml.Hadamard(wires=0)
+        qml.U3(weights[0], weights[1], weights[2], wires=0)
+        return qml.density_matrix(0)
+
+    dev = qml.device('lightning.qubit', wires=1)
+    qnode = qml.QNode(circuit_c, dev)
+
+    assert expressibility(qnode, 3, 1, n_shots=1000,
+                          method="pairwise") == pytest.approx(expressibility(qnode, 3, 1, n_shots=1000,
+                          method="full"), abs=0.1)
